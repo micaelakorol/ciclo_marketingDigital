0fb5b1e0d3ca35ef7431f78944931e57c536c8a6
Micaela Korol <micaelakorol@gmail.com>
2023-07-31 09:57:30 -0300

    changes in objetives.js

--- src/pages/GestionRedes/Objetivos/objetivos.js
+++ src/pages/GestionRedes/Objetivos/objetivos.js
@@ -8,7 +8,7 @@ export const dataGestion = {
       id: 1,
       items: "Creando una estrategia personalizada para tus publicaciones",
     },
-    { id: 2, items: "Convertiendo a tus seguidores en potenciales clientes" },
+    { id: 2, items: "Convirtiendo a tus seguidores en potenciales clientes" },
     { id: 3, items: "Estudiando a tu público objetivo" },
   ],
 };
